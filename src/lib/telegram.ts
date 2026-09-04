00640096f3595b79c154c072a178bf112daf7290
AYOMIDE JESSE <ayomidejesse7@gmail.com>
2026-09-04 18:57:49 +0100

    Restore telegram.ts (accidental overwrite)

--- src/lib/telegram.ts
+++ src/lib/telegram.ts
@@ -1,1 +1,19 @@
-RESTORE_FROM_LOCAL
+import { AsyncLocalStorage } from "node:async_hooks";
+import { scorePicks } from "./analyze.ts";
+import { parseTicketText } from "./parse-ticket.ts";
+import { normalizePidgin, pidginSmallTalk, slangHelp, splitChat, wantsCreate } from "./pidgin.ts";
+import { concentrationNote, researchPicks } from "./research.ts";
+import {
+  RULE,
+  bullets,
+  cap,
+  codeBlock,
+  doc,
+  glyph,
+  head,
+  leg,
+  pct,
+  stats,
+  subhead,
+  tail,
+} from "./tg-format.ts";
